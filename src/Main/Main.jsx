ef92ef97e6b69ac80526f28d7b98691830a49d8a
Lapat112 <lapat.41154@gmail.com>
2025-10-22 23:21:01 +0700

    front-end : build Main.jsx
    Today's work
    Make a Main page 30%
    
    Use tailwind and Css

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -6,11 +6,32 @@ import "./Main.css"
 function Main() {
   return (
     <>
-     
-      <div className="main-container ">
-        <div className="content bg-amber-400 w-[10rem] h-[10rem]"> </div>
-      </div>
-    
+      <mian className="main-container">  
+        <div className="main-container-1 ">
+          <text className="text-[28px] font-bold  absolute top-[5rem] left-[51rem] "> Algo เว็บไซต์ขายงาน</text> 
+          <text className="text-[22px] text-[#0077FF] absolute top-[8rem] left-[45rem] font-serif">เว็บไซต์สำหรับโพสขายงานต่างๆเกี่ยวกับเทคโนโลยี</text>          
+          </div> 
+        <div className="content bg-[#DA3030] w-[100%] h-[16rem] top-[13.5rem] relative"> </div>   
+          <div className="main-container-2 ">
+            <section className="advert">
+              <div className="left-box1 bg-[#D9D9D9] w-[52rem] h-[12.5rem] absolute top-[2.8rem] left-[5rem] rounded-[30px]  "></div>
+              <div className="left-box2 bg-[#D9D9D9] w-[52rem] h-[12.5rem] absolute top-[16.3rem] left-[5rem] rounded-[30px]  "></div>
+              
+              <div className="right-box1 bg-[#D9D9D9] w-[26rem] h-[26.25rem] absolute top-[2.8rem] left-[60.5rem] rounded-[30px]  "></div>
+              <div className="right-box2 bg-[#D9D9D9] w-[26rem] h-[26.25rem] absolute top-[2.8rem] left-[88rem] rounded-[30px]  "></div>
+            </section>
+
+              <div className="box-link bg-[#ffffff] border-[0.1px] w-[50rem] h-[5rem] top-[13.5rem] relative shadow-lg">
+                
+              </div>
+
+
+            </div>
+              
+
+
+
+      </mian>
     </>
   )
 }
--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -6,9 +6,9 @@ function Nav() {
   return (
     <>
       <div className="Nav-container ">
-        <text className="text-black text-[22px] top-[13px] left-[20px] absolute">Algo</text>
-        <div className="UserLogin-button bg-amber-400 w-[2.2rem] h-[2.1rem] absolute top-[13px] left-[96.5%]  rounded-full"></div>
-        <input type="text" placeholder="Search" className="Search-bar absolute text-center text-black top-[17px]  left-1/2 transform -translate-x-1/2  w-[30rem] h-[1.6rem] bg-[#EFEFEF] border-[#DD0000]  border-[1.5px] rounded-2xl"/>
+        <text className="text-black text-[24px] top-[13px] left-[20px] absolute">Algo</text>
+        <div className="UserLogin-button bg-amber-400 w-[2.5rem] h-[2.5rem] absolute top-[13px] left-[96.5%]  rounded-full"></div>
+        <input type="text" placeholder="Search" className="Search-bar absolute text-center top-[17px]  left-1/2 transform -translate-x-1/2  w-[31rem] h-[2rem] bg-[#EFEFEF] border-[#DD0000]  border-[1.5px] rounded-2xl"/>
         
       
       
