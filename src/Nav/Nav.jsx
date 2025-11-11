5dff1ca1501da50b3f2592cb8dab358e5be33ab5
Lapat112 <lapat.41154@gmail.com>
2025-11-11 13:32:47 +0700

    Add 2

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -6,9 +6,9 @@ function Nav() {
   return (
     <>
       <div className="Nav-container ">
-        <text className="text-black text-[24px] top-[13px] left-[20px] absolute">Algo</text>
-        <div className="UserLogin-button bg-amber-400 w-[2.5rem] h-[2.5rem] absolute top-[13px] left-[96.5%]  rounded-full"></div>
-        <input type="text" placeholder="Search" className="Search-bar absolute text-center top-[17px]  left-1/2 transform -translate-x-1/2  w-[31rem] h-[2rem] bg-[#EFEFEF] border-[#DD0000]  border-[1.5px] rounded-2xl"/>
+        <text className="text-black text-[20px] top-[13px] left-[20px] absolute">Algo</text>
+        <div className="Post-button w-[1.6rem] h-[1.6rem] absolute top-[17px] left-[96.5%] text-[20px]  rounded-[10px] "><img src="/src/Nav/write.png" alt="" /></div>
+        <input type="text" placeholder="Search" className="Search-bar absolute text-center top-[17px]  left-1/2 transform -translate-x-1/2  w-[31rem] h-[1.7rem] bg-[#EFEFEF] border-[#DD0000]  border-[1.5px] rounded-2xl"/>
         
       
       
