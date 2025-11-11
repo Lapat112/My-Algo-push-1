2dd3f8d1b6bba9d9bf8e39d9266266fc04e000d1
Lapat112 <lapat.41154@gmail.com>
2025-11-11 15:11:15 +0700

    Add Router
    Link Post page from Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Nav from './Nav/Nav.jsx'
-import Main from './Main/Main.jsx'
+import { BrowserRouter as Router , Routes , Route } from 'react-router-dom'
 
 import './App.css'
+import Nav from './Nav/Nav.jsx'
+import Main from './Main/Main.jsx'
+import Post from './Post/Post.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Main />
       <Nav />
+      <Routes>  
+        <Route path='/' element={<Main />}/>
+        
+        <Route path='/post' element={<Post />}/>
+      </Routes>      
+      
     </>
   )
 }
--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import{ Link } from "react-router-dom"
+
 
 import "./Nav.css"
 
@@ -6,9 +8,13 @@ function Nav() {
   return (
     <>
       <div className="Nav-container ">
-        <text className="text-black text-[20px] top-[13px] left-[20px] absolute">Algo</text>
-        <div className="Post-button w-[1.6rem] h-[1.6rem] absolute top-[17px] left-[96.5%] text-[20px]  rounded-[10px] "><img src="/src/Nav/write.png" alt="" /></div>
-        <input type="text" placeholder="Search" className="Search-bar absolute text-center top-[17px]  left-1/2 transform -translate-x-1/2  w-[31rem] h-[1.7rem] bg-[#EFEFEF] border-[#DD0000]  border-[1.5px] rounded-2xl"/>
+        <text className="text-black text-[21px] top-[13px] left-[20px] absolute">Algo</text>
+
+        <Link to="/post">
+        <div  className="Post-button w-[3rem] h-[2rem] absolute top-[17px] left-[96.5%] text-[20px]  rounded-[10px] flex justify-center items-center  "><img src="/src/Nav/write.png" className="w-[26px]" /></div>
+        </Link>
+
+        <input type="text" placeholder="Search" className="Search-bar absolute text-center top-[17px]  left-1/2 transform -translate-x-1/2  w-[31rem] h-[1.7rem] bg-[#f7f7f7] border-[#DD0000]  border-[1.5px] rounded-2xl"/>
         
       
       
